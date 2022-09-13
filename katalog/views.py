5b396578a66672a8158af9457353eb04bafc331d
CroissantAndWaffle <elrafamaritza@gmail.com>
2022-09-13 15:41:29 +0700

    -readme -test

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
-# TODO: Create your views here.
+from katalog.models import CatalogItem
+# Create your views here.
+def show_catalog(request):
+    data_catalog = CatalogItem.objects.all()
+    context = {
+        'list_catalog': data_catalog,
+        'name': 'Rafa Maritza',
+        'studentid': '2106651944',
+    }
+    return render(request, "katalog.html", context)
